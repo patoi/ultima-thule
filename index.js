a56a6a3707639383fc8bd12d654eeaa800c5a932
István Pató <istvan.pato@gmail.com>
2019-04-13 11:57:08 +0200

    add more examples

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const john = new Person({ name: 'John Doe', age: 50 })
 // TypeError: const jane = new Person('Jane Doe', 19.4)
 // RangeError: const jane = new Person('Jane Doe', 151)
 
-// Wrong type!
+// error: property type mismatch
 const jane = new Person({
     name: 'Jane Doe',
     age: '10',
@@ -28,3 +28,39 @@ console.log(isValidAge, isValidName)
 
 // NOTE: direct setting is wrong, use immutable objects, use constructor!
 // do not use: john.name = 'Jimmy Jones'
+
+/**
+ * Enum for tri-state values.
+ * @enum {number}
+ */
+const TriState = {
+    TRUE: 1,
+    FALSE: -1,
+    MAYBE: 0,
+}
+
+// code completition example
+console.log(TriState.FALSE)
+
+/**
+ * @typedef {Object} SpecialType - creates a new type named 'SpecialType'
+ * @property {string} prop1 - a string property of SpecialType
+ * @property {number} prop2 - a number property of SpecialType
+ * @property {number=} prop3 - an optional number property of SpecialType
+ * @prop {number} [prop4] - an optional number property of SpecialType
+ * @prop {number} [prop5=42] - an optional number property of SpecialType with default
+ */
+
+/** @type {SpecialType} */
+let specialTypeObject
+// error: unknown property
+specialTypeObject.prop9 = 10
+
+/**
+ * Type definition by JSDoc example.
+ * @type {Person}
+ */
+let harry
+// error: type mismatch
+harry = 10
+console.log(harry.age)
